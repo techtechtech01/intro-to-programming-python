1518062f79d42209711827c3389b8cfb62275382
techtechtech01 <167213367+techtechtech01@users.noreply.github.com>
2024-04-16 20:46:11 +0500

    Update a03.py
    Assignment done

--- assignments/a03-square-root-newton/a03.py
+++ assignments/a03-square-root-newton/a03.py
@@ -4,18 +4,27 @@
 
 
 #### YOUR CODE FOR sqrt() FUNCTION GOES HERE ####
-
+def sqrt(x,guess=0.0):
+    if average(x,a):
+        return average 
+    else:
+        return improve_guess(guess,x)
+    
 
 #### End OF MARKER
 
 #### YOUR CODE FOR average() FUNCTION GOES HERE ####
-
+def average(x,a):
+    return x+a/2.0
 
 #### End OF MARKER
 
 
 #### YOUR CODE FOR improve_guess() FUNCTION GOES HERE ####
-
+def improve_guess(guess,x):
+    x=x*1.0
+    better_guess=average(guess+x/guess)
+    return better_guess
 
 
 #### End OF MARKER
